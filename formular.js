84dfe77a66b753cd43d5a80f0706b3a0531a5e58
Canciu Costin <costin.canciu@gmail.com>
2020-04-15 18:43:28 +0300

    Update formular.js

--- formular.js
+++ formular.js
@@ -10,7 +10,7 @@ function fill(buf) {
     fields={};
     fields["nume"]="Canciu";
 	fields["prenume"]="Costin";
-	
+	alert("a");
 	var list_form = document.querySelector('.list_form');
 	var filled_pdf; // Uint8Array
 	try {
